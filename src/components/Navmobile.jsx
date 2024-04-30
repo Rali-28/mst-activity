681c7305e105d36419db4ec7b030438391944c59
Rali <113418936+Rali-28@users.noreply.github.com>
2024-04-30 23:35:32 +0800

    Complete mobile view of navbar

--- src/components/Navmobile.jsx
+++ src/components/Navmobile.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 const Navmobile = () => {
   return (
-    <div className='flex md:hidden'>Navmobile</div>
-  )
-}
+    <div className="md:hidden">
+      <Sheet side="left">
+        <SheetTrigger>Open</SheetTrigger>
+        <SheetContent side="left">
+          <SheetHeader className="text-left">
+            <SheetTitle>MENU</SheetTitle>
+            <SheetDescription>
+              <NavigationMenu>
+                <NavigationMenuList>
+                  <NavigationMenuItem className="flex flex-col">
+                    <a href="/selfcheck1" className="pt-4">
+                      <NavigationMenuLink>Self-Check 2.1.1</NavigationMenuLink>
+                    </a>
+                    <a href="/selfcheck2-a" className="pt-4">
+                      <NavigationMenuLink>Self-Check 2.2.1</NavigationMenuLink>
+                    </a>
+                    <a href="/selfcheck2-b" className="pt-4">
+                      <NavigationMenuLink>Self-Check 2.2.2</NavigationMenuLink>
+                    </a>
+                  </NavigationMenuItem>
+                </NavigationMenuList>
+              </NavigationMenu>
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
+};
 
-export default Navmobile
+export default Navmobile;
