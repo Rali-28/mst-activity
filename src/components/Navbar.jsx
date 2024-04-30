9025f7c9f8b8e3d5a250a4d17af6cc592388bdb0
Rali <113418936+Rali-28@users.noreply.github.com>
2024-05-01 00:13:29 +0800

    Refactor Navbar
    Use a separate const for rendering links
    Remove NavigationMenuLink to fix issue in rendering
    Remove toast alert and link goes straight to page
    
    Code looks much cleaner and less scattered

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,45 +2,26 @@ import React from "react";
 import {
   NavigationMenu,
   NavigationMenuItem,
-  NavigationMenuLink,
   NavigationMenuList,
   navigationMenuTriggerStyle,
   NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
-
-import { Toaster } from "@/components/ui/sonner";
-import { toast } from "sonner";
+import { links } from "./constants";
 import Navmobile from "./Navmobile";
 
 const Navbar = () => {
-  const alert = () => {
-    toast.error("Self-Check 2.2.1 not found in given module.");
-  };
-
   return (
     <nav className="px-4 py-3 w-screen backdrop-blur-sm">
-      <Toaster position="top-center" />
       <div>
         <NavigationMenu className="hidden md:flex">
           <NavigationMenuList>
-            <NavigationMenuItem>
-              <a href="/selfcheck1">
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Self-Check 2.1.1
-                </NavigationMenuLink>
-              </a>
-              <NavigationMenuLink
-                className={`cursor-pointer ${navigationMenuTriggerStyle()}`}
-                onClick={alert}
-              >
-                Self-Check 2.2.1
-              </NavigationMenuLink>
-              <a href="/selfcheck2-b">
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Self-Check 2.2.2
-                </NavigationMenuLink>
-              </a>
-            </NavigationMenuItem>
+            {links.map((link) => (
+              <NavigationMenuItem key={link.index}>
+                <a href={link.link} className={navigationMenuTriggerStyle()}>
+                    {link.title}
+                </a>
+              </NavigationMenuItem>
+            ))}
           </NavigationMenuList>
         </NavigationMenu>
         <Navmobile />
