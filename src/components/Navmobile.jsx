f7da7bd854821c5cd523e9adef34a4739671f1ac
Rali <113418936+Rali-28@users.noreply.github.com>
2024-05-01 02:33:17 +0800

    Refactor Navmobile
    Remove unused Navmenulink import
    Render links through .map function from navlinks constants

--- src/components/Navmobile.jsx
+++ src/components/Navmobile.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import {
   NavigationMenu,
   NavigationMenuItem,
-  NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
@@ -14,6 +13,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { links } from "./constants";
 
 const Navmobile = () => {
   return (
@@ -23,22 +23,12 @@ const Navmobile = () => {
         <SheetContent side="left">
           <SheetHeader className="text-left">
             <SheetTitle>MENU</SheetTitle>
-            <SheetDescription>
-              <NavigationMenu>
-                <NavigationMenuList>
-                  <NavigationMenuItem className="flex flex-col">
-                    <a href="/selfcheck1" className="pt-4">
-                      <NavigationMenuLink>Self-Check 2.1.1</NavigationMenuLink>
-                    </a>
-                    <a href="/selfcheck2-a" className="pt-4">
-                      <NavigationMenuLink>Self-Check 2.2.1</NavigationMenuLink>
-                    </a>
-                    <a href="/selfcheck2-b" className="pt-4">
-                      <NavigationMenuLink>Self-Check 2.2.2</NavigationMenuLink>
-                    </a>
-                  </NavigationMenuItem>
-                </NavigationMenuList>
-              </NavigationMenu>
+            <SheetDescription className="flex flex-col">
+              {links.map((link) => (
+                <a key={link.index} href={link.link} className="pt-4">
+                  {link.title}
+                </a>
+              ))}
             </SheetDescription>
           </SheetHeader>
         </SheetContent>
