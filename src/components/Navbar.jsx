656b61a7acafd4a5eb56323421211dad6252df65
Rali <113418936+Rali-28@users.noreply.github.com>
2024-04-30 19:52:32 +0800

    Create a render of the links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <a href="/activity1">
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Activity 2.1.1
+              </NavigationMenuLink>
+            </a>
+            <a href="/activity2-a">
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Activity 2.1.1
+              </NavigationMenuLink>
+            </a>
+            <a href="/activity2-b">
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Activity 2.1.1
+              </NavigationMenuLink>
+            </a>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
