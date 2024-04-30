2d5095f63f8d452cd22f083dd2b4853638dd0555
Rali <113418936+Rali-28@users.noreply.github.com>
2024-04-30 23:49:06 +0800

    Create navlinks constant

---
+++ src/components/constants/navLinks.js
@@ -0,0 +1,17 @@
+export const links = [
+    {
+        index: 1,
+        title: "Self-Check 2.1.1",
+        link: "/selfcheck1",
+    },
+    {
+        index: 2,
+        title: "Self-Check 2.2.1",
+        link: "/selfcheck2-a",
+    },
+    {
+        index: 3,
+        title: "Self-Check 2.2.2",
+        link: "/selfcheck2-b",
+    },
+]
