7501f067c1be9f12c80338a35fe4847f66fe1e1f
Rali <113418936+Rali-28@users.noreply.github.com>
2024-05-01 20:25:16 +0800

    Create a reusable sonner component for use

---
+++ src/components/Sonner.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Toaster } from './ui/sonner'
+import { toast } from 'sonner'
+import { Button } from './ui/button'
+
+const Sonner = ({title , msg}) => {
+  return (
+    <div>
+        <Toaster position='top-center' />
+        <Button
+        variant="outline"
+        onClick={() =>
+          toast(msg)
+        }
+      >
+        {title}
+      </Button>
+    </div>
+  )
+}
+
+export default Sonner
