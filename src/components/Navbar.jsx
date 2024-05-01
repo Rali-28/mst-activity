d082b345ff400853ddc8ad7f4339e54730443225
Rali <113418936+Rali-28@users.noreply.github.com>
2024-05-01 13:56:24 +0800

    Add a header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,20 +13,23 @@ import { ModeToggle } from "./ModeToggle";
 const Navbar = () => {
   return (
     <nav className="px-6 py-3 w-screen backdrop-blur-sm">
-      <div className="flex justify-between items-center">
-        <NavigationMenu className="hidden md:flex">
-          <NavigationMenuList>
-            {links.map((link) => (
-              <NavigationMenuItem key={link.index}>
-                <a href={link.link} className={navigationMenuTriggerStyle()}>
+      <div className="flex justify-between">
+        <div className="hidden md:flex">
+          <a href="/" className={navigationMenuTriggerStyle()}>Janaban</a>
+          <NavigationMenu className="hidden md:flex flex-">
+            <NavigationMenuList className="flex">
+              {links.map((link) => (
+                <NavigationMenuItem key={link.index}>
+                  <a href={link.link} className={navigationMenuTriggerStyle()}>
                     {link.title}
-                </a>
-              </NavigationMenuItem>
-            ))}
-          </NavigationMenuList>
-        </NavigationMenu>
-         <Navmobile />
-         <ModeToggle/>   
+                  </a>
+                </NavigationMenuItem>
+              ))}
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+        <Navmobile />
+        <ModeToggle />
       </div>
     </nav>
   );
