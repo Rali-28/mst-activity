f36cc539d5ad4c4db10fdcf3d8a688f63b5f5058
Rali <113418936+Rali-28@users.noreply.github.com>
2024-05-02 23:17:26 +0800

    Add radio group

--- src/components/MultipleChoices.jsx
+++ src/components/MultipleChoices.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 const MultipleChoices = () => {
   return (
-    <div>MultipleChoices</div>
-  )
-}
+    <div className="mx-auto w-1/2">
+      <div>
+        <span>Text</span>
+        <RadioGroup defaultValue="comfortable">
+          <div className="flex items-center space-x-2">
+            <RadioGroupItem value="option-one" id="option-one" disabled />
+            <Label htmlFor="option-one">Option 1</Label>
+          </div>
 
-export default MultipleChoices
+          <div className="flex space-x-2">
+            <RadioGroupItem value="option-two" id="option-two" checked />
+            <Label htmlFor="option-two">Option 2</Label>
+          </div>
+
+          <div className="flex space-x-2">
+            <RadioGroupItem value="option-three" id="option-three" disabled />
+            <Label htmlFor="option-three">
+              Payag ka non, option ka langa sdsd sda das s asd asd sdasdaasd asd
+              das das?
+            </Label>
+          </div>
+
+          <div className="flex items-center space-x-2">
+            <RadioGroupItem value="option-four" id="option-four" disabled />
+            <Label htmlFor="option-four">Payag ka non, option ka lang?</Label>
+          </div>
+        </RadioGroup>
+      </div>
+    </div>
+  );
+};
+
+export default MultipleChoices;
