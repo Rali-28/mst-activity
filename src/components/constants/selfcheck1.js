fc9c42501dbcc2c87b38f9b8187343fa646fbbb8
Rali <113418936+Rali-28@users.noreply.github.com>
2024-05-03 19:50:59 +0800

    Create constants for selfcheck activity

---
+++ src/components/constants/selfcheck1.js
@@ -0,0 +1,113 @@
+export const activity = [
+  {
+    index: 1,
+    question:
+      "This is the type of lathe that is usually mounted on a bench and designed for small works.",
+    choices: {
+      a: { choice: "Engine lathe", state: "disabled" },
+      b: { choice: "Turret lathe", state: "disabled" },
+      c: { choice: "bench type lathe", state: "checked" },
+      d: { choice: "quick change gear", state: "disabled" },
+    },
+  },
+  {
+    index: 2,
+    question:
+      "The sliding part of the lathe that provides support for long and slender work piece is called,",
+    choices: {
+      a: { choice: "Tail stock", state: "checked" },
+      b: { choice: "Carriage	", state: "disabled" },
+      c: { choice: "bed", state: "disabled" },
+      d: { choice: "head stock", state: "disabled" },
+    },
+  },
+  {
+    index: 3,
+    question: "To move the carriage by hand we use this part of the carriage,",
+    choices: {
+      a: { choice: "Cross slide", state: "checked" },
+      b: { choice: "Compound rest", state: "disabled" },
+      c: { choice: "tool post", state: "disabled" },
+      d: { choice: "apron hand wheel", state: "disabled" },
+    },
+  },
+  {
+    index: 4,
+    question:
+      "This part of the lathe is carefully and accurately machined to keep the tail stock and head stock aligned,",
+    choices: {
+      a: { choice: "Carriage", state: "disabled" },
+      b: { choice: "Lathe bed", state: "checked" },
+      c: { choice: "saddle", state: "disabled" },
+      d: { choice: "cross slide", state: "disabled" },
+    },
+  },
+  {
+    index: 5,
+    question:
+      "5.The primary function of this machine is to remove metal from a piece of work that is made to revolve by feeding the tool along the surface.",
+    choices: {
+      a: { choice: "Head stock", state: "disabled" },
+      b: { choice: "Chuck", state: "disabled" },
+      c: { choice: "lathe machine", state: "checked" },
+      d: { choice: "spindle", state: "disabled" },
+    },
+  },
+  {
+    index: 6,
+    question:
+      "It has a standard change gear box with set of independent change gears that connects spindle to lead screw of the lathe,",
+    choices: {
+      a: { choice: "Standard  change gear lathe", state: "checked" },
+      b: { choice: "Turret lathe", state: "disabled" },
+      c: { choice: "engine lathe", state: "disabled" },
+      d: { choice: "quick change gear lathe", state: "disabled" },
+    },
+  },
+  {
+    index: 7,
+    question:
+      "This is the rotating part of the lathe that carries the chuck and work piece in rotary motion,",
+    choices: {
+      a: { choice: "Tail stock", state: "disabled" },
+      b: { choice: "Spindle", state: "checked" },
+      c: { choice: "tool post ", state: "disabled" },
+      d: { choice: "compound rest", state: "disabled" },
+    },
+  },
+  {
+    index: 8,
+    question:
+      "It has gear box in which the gears used to obtain the desired feed and TPI or pitch of thread are so arranged. We need to shift a lever or two to change position of gears.",
+    choices: {
+      a: { choice: "Turret lathe machine", state: "disabled" },
+      b: { choice: "Bench type lathe", state: "disabled" },
+      c: { choice: "engine lathe machine", state: "disabled" },
+      d: { choice: "quick change gear lathe", state: "checked" },
+    },
+  },
+  {
+    index: 9,
+    question:
+      "The part of the tail stock used to feed in the drill when drilling in the lathe,",
+    choices: {
+      a: { choice: "Tail stock hand wheel	", state: "checked" },
+      b: { choice: "Tail stock spindle", state: "disabled" },
+      c: { choice: "tail stock lock lever", state: "disabled" },
+      d: { choice: "spindle lock", state: "disabled" },
+    },
+  },
+  {
+    index: 10,
+    question:
+      "The lathe machine that is mounted directly to the floor and designed for large works.",
+    choices: {
+      a: { choice: "Turret lathe", state: "disabled" },
+      b: { choice: "Bench lathe", state: "disabled" },
+      c: { choice: "engine lathe", state: "checked" },
+      d: { choice: "standard change gear lathe", state: "disabled" },
+    },
+  },
+];
+
+
