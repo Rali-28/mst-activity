bc77e7bc6efe82c771fd49d35828bb3b44fd1850
Rali <113418936+Rali-28@users.noreply.github.com>
2024-05-03 22:53:52 +0800

    Export selfcheck2a

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export {default as Navbar} from "./Navbar.jsx"
 export {default as Sonner} from "./Sonner.jsx"
 export {default as MultipleChoices} from "./SelfCheck1.jsx"
+export {default as SelfCheck2A} from "./SelfCheck2a.jsx"
