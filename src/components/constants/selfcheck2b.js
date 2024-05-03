33d3ce2bd3d9018efc86306d39305e01c2bc3de3
Rali <113418936+Rali-28@users.noreply.github.com>
2024-05-04 00:08:59 +0800

    Create constants for  selfcheck 2b activity
    No answers yet

---
+++ src/components/constants/selfcheck2b.js
@@ -0,0 +1,24 @@
+export const activity = [
+  {
+    index: 1,
+    instruction: "I.Calculate the RPM for the following materials in inch sizes:",
+    problem: {
+        1: {material: "Material: Mild steel round bar 3/8\" diameter", ans: "N/A"},
+        2: {material: "Material: Copper round bar 5/8\" diameter", ans: "N/A"},
+        3: {material: "Material: Aluminum round bar 1/4\" diameter", ans: "N/A"},
+        4: {material: "Material: Cast iron round bar 3/4\" diameter", ans: "N/A"},
+        5: {material: "Material: Tool steel, drill bit 5/16\" diameter", ans: "N/A"},
+    },
+  },
+  {
+    index: 2,
+    instruction: "II.Calculate the RPM for the following materials in mm size:",
+    materials: {
+        1: {material: "Material: Mild steel round bar 10mm diameter", ans: "N/A"},
+        2: {material: "Material: Copper round bar 25mm diameter", ans: "N/A"},
+        3: {material: "Material: Aluminum round bar 16mm diameter", ans: "N/A"},
+        4: {material: "Material: Cast iron round bar 8mm diameter", ans: "N/A"},
+        5: {material: "Material: Tool steel round bar 14mm diameter", ans: "N/A"},
+    },
+  },
+];
