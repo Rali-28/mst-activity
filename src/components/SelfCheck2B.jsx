8f3c2c30fb92c7f17455b34bc21c0d9c35111c4e
Rali <113418936+Rali-28@users.noreply.github.com>
2024-05-04 21:58:31 +0800

    Create layout for render

--- src/components/SelfCheck2B.jsx
+++ src/components/SelfCheck2B.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   Table,
   TableBody,
@@ -7,12 +7,79 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
+} from "@/components/ui/table";
 
 const SelfCheck2B = () => {
   return (
-    <div>SelfCheck2B</div>
-  )
-}
+    <div className="mx-auto w-1/2">
+      <ol className="list-[upper-roman]">
+        <li className="mb-4">
+          Header 1
+          <Table>
+            <TableCaption>
+              Imperial Material List and cutting speed
+            </TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-1/2 text-center">Material</TableHead>
+                <TableHead className="text-center">Cutting Speed</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell className="border-x-[1px]">mat1</TableCell>
+                <TableCell className="border-x-[1px]">cs1</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="border-x-[1px]">mat2</TableCell>
+                <TableCell className="border-x-[1px]">cs2</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+          <ol className="ml-4 list-decimal">
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+            <li>Item 5</li>
+            <li>Item 6</li>
+          </ol>
+        </li>
+        <li className="mb-4">
+          Header 2
+          <Table>
+            <TableCaption>
+              Imperial Material List and cutting speed
+            </TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-1/2 text-center">Material</TableHead>
+                <TableHead className="text-center">Cutting Speed</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell className="border-x-[1px]">mat1</TableCell>
+                <TableCell className="border-x-[1px]">cs1</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="border-x-[1px]">mat2</TableCell>
+                <TableCell className="border-x-[1px]">cs2</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+          <ol className="ml-4 list-decimal">
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+            <li>Item 5</li>
+            <li>Item 6</li>
+          </ol>
+        </li>
+      </ol>
+    </div>
+  );
+};
 
-export default SelfCheck2B
+export default SelfCheck2B;
