256f9a3d99fa741b6991475b7f6168f48b44f1e6
Rali <113418936+Rali-28@users.noreply.github.com>
2024-05-04 19:25:52 +0800

    Combine constants for chart

--- src/components/constants/selfcheck2b.js
+++ src/components/constants/selfcheck2b.js
@@ -1,57 +1,74 @@
-export const imperialChart =[
-    {
-        index: 1,
-        material: "Mild Steel",
-        cs: "80 ft/min",
-    },
-    {
-        index: 2,
-        material: "Tool Steel",
-        cs: "60 ft/min",
-    },
-    {
-        index: 3,
-        material: "Stainless Steel",
-        cs: "50 ft/min",
-    },
-    {
-        index: 4,
-        material: "Cast Iron",
-        cs: "80 ft/min",
-    },
-    {
-        index: 5,
-        material: "Copper and Alloys of Copper",
-        cs: "200 ft/min",
-    },
-    {
+export const chart = [
+  {
+    index: 1,
+    formula: "",
+    imperial: [
+      { index: 1, material: "Mild Steel", cs: "80 ft/min" },
+      { index: 2, material: "Tool Steel", cs: "60 ft/min" },
+      { index: 3, material: "Stainless Steel", cs: "50 ft/min" },
+      { index: 4, material: "Cast Iron", cs: "80 ft/min" },
+      { index: 5, material: "Copper and Alloys of Copper", cs: "200 ft/min" },
+      {
         index: 6,
         material: "Aluminum and Alloys of Aluminum",
         cs: "300 ft/min",
-    },
-]
+      },
+    ],
+  },
+  {
+    index: 1,
+    formula: "",
+    metric: [
+      { index: 1, material: "Mild Steel", cs: "27 m/min" },
+      { index: 2, material: "Tool Steel", cs: "20 m/min" },
+      { index: 3, material: "Stainless Steel", cs: "17 m/min" },
+      { index: 4, material: "Cast Iron", cs: "27 m/min" },
+      { index: 5, material: "Copper and Alloys of Copper", cs: "67 m/min" },
+      {
+        index: 6,
+        material: "Aluminum and Alloys of Aluminum",
+        cs: "100 m/min",
+      },
+    ],
+  },
+];
 
 export const activity = [
   {
     index: 1,
     instruction: "Calculate the RPM for the following materials in inch sizes:",
     problem: {
-        1: {material: "Material: Mild steel round bar 3/8\" diameter", ans: "N/A"},
-        2: {material: "Material: Copper round bar 5/8\" diameter", ans: "N/A"},
-        3: {material: "Material: Aluminum round bar 1/4\" diameter", ans: "N/A"},
-        4: {material: "Material: Cast iron round bar 3/4\" diameter", ans: "N/A"},
-        5: {material: "Material: Tool steel, drill bit 5/16\" diameter", ans: "N/A"},
+      1: {
+        material: 'Material: Mild steel round bar 3/8" diameter',
+        ans: "N/A",
+      },
+      2: { material: 'Material: Copper round bar 5/8" diameter', ans: "N/A" },
+      3: { material: 'Material: Aluminum round bar 1/4" diameter', ans: "N/A" },
+      4: {
+        material: 'Material: Cast iron round bar 3/4" diameter',
+        ans: "N/A",
+      },
+      5: {
+        material: 'Material: Tool steel, drill bit 5/16" diameter',
+        ans: "N/A",
+      },
     },
   },
   {
     index: 2,
     instruction: "Calculate the RPM for the following materials in mm size:",
     materials: {
-        1: {material: "Material: Mild steel round bar 10mm diameter", ans: "N/A"},
-        2: {material: "Material: Copper round bar 25mm diameter", ans: "N/A"},
-        3: {material: "Material: Aluminum round bar 16mm diameter", ans: "N/A"},
-        4: {material: "Material: Cast iron round bar 8mm diameter", ans: "N/A"},
-        5: {material: "Material: Tool steel round bar 14mm diameter", ans: "N/A"},
+      1: {
+        material: "Material: Mild steel round bar 10mm diameter",
+        ans: "N/A",
+      },
+      2: { material: "Material: Copper round bar 25mm diameter", ans: "N/A" },
+      3: { material: "Material: Aluminum round bar 16mm diameter", ans: "N/A" },
+      4: { material: "Material: Cast iron round bar 8mm diameter", ans: "N/A" },
+      5: {
+        material: "Material: Tool steel round bar 14mm diameter",
+        ans: "N/A",
+      },
     },
   },
 ];
