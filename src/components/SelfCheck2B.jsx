0bf46b75a5413dfa726039a278cf47aa3c4cd90b
Rali <113418936+Rali-28@users.noreply.github.com>
2024-05-05 19:09:54 +0800

    Render instruction list

--- src/components/SelfCheck2B.jsx
+++ src/components/SelfCheck2B.jsx
@@ -9,11 +9,18 @@ import {
   TableRow,
   TableFooter,
 } from "@/components/ui/table";
-import {chart , activity2b} from "./constants/"
+import { chart, activity2b } from "./constants/";
 
 const SelfCheck2B = () => {
   return (
     <div className="mx-auto w-1/2">
+      <ol className="list-[upper-roman]">
+        {activity2b.map((activity) => (
+          <li key={activity.index}>
+            {activity.instruction}
+          </li>
+        ))}
+      </ol>
       <ol className="list-[upper-roman]">
         <li className="mb-4">
           Header 1
@@ -21,7 +28,8 @@ const SelfCheck2B = () => {
             <TableCaption>
               <p>
                 Imperial Measurement of Material List and Cutting Speed <br />
-                (Note: Cutting speeds for tools of High Carbon Steel is ½ of CS for HSS) 
+                (Note: Cutting speeds for tools of High Carbon Steel is ½ of CS
+                for HSS)
               </p>
             </TableCaption>
             <TableHeader>
@@ -44,7 +52,7 @@ const SelfCheck2B = () => {
               <TableRow className="text-right">
                 <TableCell>Formula:</TableCell>
                 <TableCell className="flex justify-start items-center">
-                \[RPM =   \]\[cs * 4 \over diamater  \]
+                  \[RPM = \]\[cs * 4 \over diamater \]
                 </TableCell>
               </TableRow>
             </TableFooter>
@@ -62,8 +70,8 @@ const SelfCheck2B = () => {
           Header 2
           <Table>
             <TableCaption>
-              Metric Measurements of Material List and Cutting Speed
-              (Note: Cutting speeds for tools of High Carbon Steel is ½ of CS for HSS) 
+              Metric Measurements of Material List and Cutting Speed (Note:
+              Cutting speeds for tools of High Carbon Steel is ½ of CS for HSS)
             </TableCaption>
             <TableHeader>
               <TableRow>
@@ -85,7 +93,7 @@ const SelfCheck2B = () => {
               <TableRow className="text-right">
                 <TableCell>Formula:</TableCell>
                 <TableCell className="flex justify-start items-center">
-                \[RPM =   \]\[cs * 1000 \over \pi * diamater  \]
+                  \[RPM = \]\[cs * 1000 \over \pi * diamater \]
                 </TableCell>
               </TableRow>
             </TableFooter>
