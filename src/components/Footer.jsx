cb65cc4648f5b34415cf9eb3805b7c6ab8b9ab73
Rali <113418936+Rali-28@users.noreply.github.com>
2024-05-15 23:24:40 +0800

    Create the footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="w-screen py-10 pl-9 mt-4 text-left border-t-[1px]">
+      Create by: Sean "Rali" Janaban
+    </div>
+  )
+}
+
+export default Footer
